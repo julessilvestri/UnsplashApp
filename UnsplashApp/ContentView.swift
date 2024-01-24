0c2595f6c384d8e8163a6d5d929821837c75494a
julessilvestri <jules.sil05@gmail.com>
2024-01-24 08:36:28 +0100

    FeedState

--- UnsplashApp/ContentView.swift
+++ UnsplashApp/ContentView.swift
@@ -6,99 +6,93 @@
 //
 
 import SwiftUI
+import Foundation
+
+struct UnsplashPhoto: Codable, Identifiable {
+    let id: String
+    let slug: String
+    let author: User
+    let url: UnsplashPhotoUrls
+    
+    enum CodingKeys: String, CodingKey {
+        case id, slug
+        case author = "user"
+        case url = "urls"
+    }
+}
+
+struct User: Codable {
+    let name: String
+    
+    enum CodingKeys: String, CodingKey{
+        case name
+    }
+}
+
+struct UnsplashPhotoUrls: Codable {
+    let raw: String
+    let full: String
+    let regular: String
+    let small: String
+    let thumb: String
+}
 
 struct ContentView: View {
-    
     let columns = [
         GridItem(.flexible(minimum: 150)),
         GridItem(.flexible(minimum: 150))
     ]
-    
-    struct UnsplashPhoto: Codable, Identifiable {
-        let id: String
-        let slug: String
-        let author: User
-        let url: UnsplashPhotoUrls
-        
-        enum CodingKeys: String, CodingKey {
-            case id, slug
-            case author = "user"
-            case url = "urls"
-        }
-    }
-    
-    struct User: Codable {
-        let name: String
-        
-        enum CodingKeys: String, CodingKey{
-            case name
-        }
-    }
-    
-    struct UnsplashPhotoUrls: Codable {
-        let raw: String
-        let full: String
-        let regular: String
-        let small: String
-        let thumb: String
-    }
-    
-    // Déclaration d'une variable d'état, une fois remplie, elle va modifier la vue
+
     @State var imageList: [UnsplashPhoto] = []
+    @StateObject var feedState = FeedState()
     
-    // Déclaration d'une fonction asynchrone
-    func loadData() async {
-        // Créez une URL avec la clé d'API
-        let url = URL(string: "https://api.unsplash.com/photos?client_id=\(ConfigurationManager.instance.plistDictionnary.clientId)")!
-        
-        do {
-            // Créez une requête avec cette URL
-            let request = URLRequest(url: url)
-            
-            // Faites l'appel réseau
-            let (data, _) = try await URLSession.shared.data(for: request)
-            
-            // Transformez les données en JSON
-            let deserializedData = try JSONDecoder().decode([UnsplashPhoto].self, from: data)
-            
-            // Mettez à jour l'état de la vue
-            imageList = deserializedData
-            
-        } catch {
-            print("Error: \(error)")
-        }
-    }
-    
-    // Créez cette nouvelle structure visuelle
     var body: some View {
         NavigationStack{
             VStack {
-                // le bouton va lancer l'appel réseau
                 Button(action: {
                     Task {
-                        await loadData()
+                        await feedState.fetchHomeFeed()
                     }
                 }, label: {
                     Text("Load Data")
                 })
-                ScrollView {
-                    LazyVGrid(columns: columns, spacing: 8){
-                        ForEach(imageList) { image in
-                            AsyncImage(url: URL(string: image.url.regular)){ image in
-                                image.resizable()
-                            } placeholder: {
-                                ProgressView()
+                if let homeFeed = feedState.homeFeed {
+                    ScrollView {
+                        LazyVGrid(columns: columns, spacing: 8){
+                            ForEach(homeFeed) { image in
+                                AsyncImage(url: URL(string: image.url.regular)){ image in
+                                    image.resizable()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(height: 150)
+                                .cornerRadius(12)
                             }
-                            .frame(height: 150)
-                            .cornerRadius(12)
                         }
                     }
+                    .padding(.horizontal)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    .navigationBarTitle("Feed")
                 }
-                .padding(.horizontal)
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-                .navigationBarTitle("Feed")
+                else{
+                    ScrollView {
+                        LazyVGrid(columns: columns, spacing: 8){
+                            ForEach(0..<12, id: \.self) { image in
+                                Rectangle()
+                                .frame(height: 150)
+                                .cornerRadius(12)
+                                .foregroundColor(.gray)
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    .redacted(reason: .placeholder)
+                    .navigationBarTitle("Feed")
+                }
             }
-        }}
+        }
+    }
 }
 
 
--- UnsplashApp/FeedState.swift
+++ UnsplashApp/FeedState.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+class FeedState: ObservableObject {
+    @Published var homeFeed: [UnsplashPhoto]?
+
+    func fetchHomeFeed() async {
+        do {
+            if let url = UnsplashAPI.feedUrl() {
+                let request = URLRequest(url: url)
+                let (data, _) = try await URLSession.shared.data(for: request)
+                let deserializedData = try JSONDecoder().decode([UnsplashPhoto].self, from: data)
+                DispatchQueue.main.async {
+                    self.homeFeed = deserializedData
+                }
+            }
+        } catch {
+            print("Error fetching photo feed: (error)")
+        }
+    }
+}
